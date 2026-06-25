c7e51e877989c95ea6dc4eb86217b059e9c29716
204g1a0551 <204g1a0551@srit.ac.in>
2026-06-25 20:30:38 +0530

    feat: add PeekViewDirective, its test file, and initial global files for application structure

---
+++ compliance-app/src/app/shared/directive/peek-view.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PeekViewDirective } from './peek-view.directive';
+
+describe('PeekViewDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PeekViewDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ compliance-app/src/app/shared/directive/peek-view.directive.ts
@@ -0,0 +1,6 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appPeekViewDirective]',
+})
+export class PeekViewDirective {}
